16afc7f7ef1e98e81b3e40d4a619bc67ff426230
Eimar <eimar.urbina@gmail.com>
2019-09-30 07:53:28 -0400

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
 //Web Store
 
 Auth::routes();
